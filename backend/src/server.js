1cd3ce84d44729f23afd075eb06023e0c846f2f0
deepak-linkedin <deepakbanda2005@gmail.com>
2026-05-19 17:27:42 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,7 +12,7 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001;
-
+const __dirname = path.resolve();
 
 //middleware
 //sample custom middleware
@@ -19,17 +20,27 @@ const PORT = process.env.PORT || 5001;
 //     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
 //     next();
 // });
-app.use(
+if(process.env.NODE_ENV !== "production"){
+    app.use(
     cors({
         origin: "http://localhost:5173",
     })
 );
+}
+
 app.use(express.json())
 app.use(rateLimiter);
 //app.use(cors());
 
 app.use("/api/notes",notesRoutes);
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))); // dist is optimized react application
+    // if we get any route other than our designated routes(/api/notes/)
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 connectDB().then(()=>{
     app.listen(PORT,() => {
